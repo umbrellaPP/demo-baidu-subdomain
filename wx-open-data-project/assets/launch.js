f7b8c494679b870f4e1d9d1e02bd9dc632c191d6
PP <chinese1236@qq.com>
2018-12-07 00:05:42 +0800

    Initial commit

---
+++ wx-open-data-project/assets/launch.js
@@ -0,0 +1,73 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        content: cc.Node,
+        prefab: cc.Prefab
+    },
+
+    start () {
+        let _self = this;
+
+        swan.onMessage( data => {
+            console.log(data.message);
+        });
+
+        // https://developers.weixin.qq.com/minigame/dev/document/open-api/data/swan.getUserInfo.html
+        swan.getUserInfo({
+            swanIdList: ['selfSwanId'],
+            success: (res) => {
+                console.log('success', res.data);
+                let userInfo = res.data[0];
+                _self.createUserBlock(userInfo);
+            },
+            fail: (res) => {
+                reject(res);
+            }
+        });
+        
+        // https://developers.weixin.qq.com/minigame/dev/document/open-api/data/swan.getFriendCloudStorage.html
+        swan.getFriendCloudStorage({
+            keyList: ['key'],
+            success: function (res) {
+                for (let i = 0; i < 3; i++) {
+                    let friendInfo = res.data[i];
+                    if (!friendInfo) {
+                        _self.createPrefab();
+                        continue;
+                    }
+                    _self.createUserBlock(friendInfo);
+                }
+            },
+            fail: function (res) {
+                console.error(res);
+            }
+        });
+    },
+
+    createUserBlock (user) {
+        let node = this.createPrefab();
+        // getUserInfo will return the nickName, getFriendCloudStorage will return the nickname.
+        let nickName = user.nickName ? user.nickName : user.nickname;
+        let avatarUrl = user.avatarUrl;
+
+        let userName = node.getChildByName('userName').getComponent(cc.Label);
+        let userIcon = node.getChildByName('mask').children[0].getComponent(cc.Sprite);
+
+        userName.string = nickName;
+        console.log(nickName + '\'s info has been getten.');
+        cc.loader.load({
+            url: avatarUrl, type: 'png'
+        }, (err, texture) => {
+            if (err) console.error(err);
+            userIcon.spriteFrame = new cc.SpriteFrame(texture);
+        });                   
+    },
+
+    createPrefab () {
+        let node = cc.instantiate(this.prefab);
+        node.parent = this.content;
+        return node;
+    }
+
+});
